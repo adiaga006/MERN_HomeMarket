18d928b5ba35488e7e6fc5d9eceefd321ca50cac
Nguyễn Minh Tuấn <81579690+Tuanght@users.noreply.github.com>
2023-11-24 01:40:52 +0700

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb://0.0.0.0:27017/Ecommerce", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
