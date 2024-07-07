71359e2b5857f760c7d743747642a6060d366046
adiaga006 <85040268+adiaga006@users.noreply.github.com>
2024-07-07 16:29:28 +0700

    fixx

--- server/app.js
+++ server/app.js
@@ -49,7 +49,7 @@ CreateAllFolder();
 
 // Database Connection
 mongoose
-  .connect(process.env.DB_LOCAL, {
+  .connect(process.env.DB_CLOUD, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
